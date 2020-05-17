5aa99388f766543174e0aebacf0cccb2c96dcda6
Saul-Ordonez <sauleordonez@gmail.com>
2020-05-17 14:48:57 -0700

    add API request code to addBlogPost function

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
-import IndexScreen from './src/screens/IndexScreens';
+import IndexScreen from './src/screens/IndexScreen';
 import { Provider } from './src/context/BlogContext';
 import ShowScreen from './src/screens/ShowScreen';
 import CreateScreen from './src/screens/CreateScreen';
